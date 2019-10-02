4bad2e605fa665ca486b6236475c0e2f2179f788
Wangmumu1009 <18846128334@163.com>
2019-10-02 09:46:40 +0800

    首页布局及vant使用

--- src/main.js
+++ src/main.js
@@ -8,6 +8,16 @@ Vue.config.productionTip = false
 import '@/assets/css/reset.css'//@指的是src文件
 import '@/assets/js/rem.js'
 
+//按需引入vant组件 推荐
+import { Button, Icon, NavBar, Tabbar, TabbarItem, Swipe, SwipeItem, Lazyload   } from 'vant';
+Vue.use(Button).use(Icon).use(NavBar).use(Tabbar).use(TabbarItem).use(Swipe).use(SwipeItem).use(Lazyload);
+
+//vant的完全引入，也就是引入所有的组件，缺点是体积大
+// import Vant from 'vant';
+// import 'vant/lib/index.css';
+
+// Vue.use(Vant);
+
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,63 @@
 <template>
-  <div>首页</div>
+  <div>
+    <van-nav-bar title="商城">
+      <van-icon name="search" slot="left" />
+      <van-icon name="cart" slot="right" />
+    </van-nav-bar>
+    <!-- 轮播图 -->
+    <div>
+      <van-swipe :autoplay="3000">
+        <van-swipe-item class="carousel-item" v-for="(item,index) in carouselItem" :key="index">
+          <img v-lazy="item.imgSrc" alt />
+          <!-- 是：src的时候全加载出来，性能会不好 -->
+        </van-swipe-item>
+      </van-swipe>
+    </div>
+
+    <!-- 热门商品 -->
+    <div class="hot">
+      <p>热门商品</p>
+
+    </div>
+    <div>
+
+    </div>
+  </div>
 </template>
 
 <script>
-
-export default{
-
-}
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+  data() {
+    return {
+      carouselItem: [
+        {
+          name: "img1",
+          imgSrc: "cd"
+        },
+        {
+          name: "img2",
+          imgSrc: "cd"
+        },
+        {
+          name: "img2",
+          imgSrc: "cd"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="scss">
-
+.carousel {
+  height: 3rem;
+  &-item {
+    img {
+      width: 100rem;
+      height: 3rem;
+    }
+  }
+}
 </style>
